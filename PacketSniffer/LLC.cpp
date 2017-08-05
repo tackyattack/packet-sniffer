de4db983447e97fc2b6a63c3a11247a475623360
Henry Bergin <tackyattack@gmail.com>
2017-08-04 22:50:13 -0500

    handshake detection

--- PacketSniffer/LLC.cpp
+++ PacketSniffer/LLC.cpp
@@ -43,8 +43,14 @@ void send_LLC_UP(LLC_PDU LLC)
 void send_LLC_SNAP_UP(LLC_PDU_SNAP LLC)
 { // send the LLC up to the correct layer
     printf("\n------LLC SNAP------\n");
-    uint16_t eth_type = LLC.ether_type[0] | ((LLC.ether_type[1]) << 8);
+    uint16_t eth_type = LLC.ether_type[1] | ((LLC.ether_type[0]) << 8); //
     printf("type: %04X\n",eth_type);
+    
+    if(eth_type == 0x888E)
+    { // 802.1X Auth
+        printf("AUTH!!!!!!!\n");
+    }
+    
 }
 
 void process_LLC(const u_char *frame_start, uint16_t data_length)
@@ -86,7 +92,7 @@ void process_LLC(const u_char *frame_start, uint16_t data_length)
     if(LLC_type == LLC_TYPE_SNAP)
     {
         memcpy(&(LLC_SNAP.DSAP), frame_start, DSAP_SIZE);
-        memcpy(&(LLC_SNAP.DSAP), frame_start + DSAP_SIZE, SSAP_SIZE);
+        memcpy(&(LLC_SNAP.SSAP), frame_start + DSAP_SIZE, SSAP_SIZE);
         if(LLC_control_size == CONTROL_SIZE_16)
         {
             memcpy(&(LLC_SNAP.control), frame_start + DSAP_SIZE + SSAP_SIZE, LLC_control_size);
@@ -98,7 +104,7 @@ void process_LLC(const u_char *frame_start, uint16_t data_length)
         }
         
          memcpy(&(LLC_SNAP.org_code), frame_start + DSAP_SIZE + SSAP_SIZE + LLC_control_size, ORG_SIZE);
-        memcpy(&(LLC_SNAP.org_code), frame_start + DSAP_SIZE + SSAP_SIZE + LLC_control_size + ORG_SIZE, ETH_SIZE);
+        memcpy(&(LLC_SNAP.ether_type), frame_start + DSAP_SIZE + SSAP_SIZE + LLC_control_size + ORG_SIZE, ETH_SIZE);
         
         LLC_SNAP.data_start = frame_start + DSAP_SIZE + SSAP_SIZE + LLC_control_size + ORG_SIZE + ETH_SIZE;
         LLC_SNAP.data_length = data_length - (DSAP_SIZE + SSAP_SIZE + LLC_control_size + ORG_SIZE + ETH_SIZE);
@@ -107,7 +113,7 @@ void process_LLC(const u_char *frame_start, uint16_t data_length)
     else if(LLC_type == LLC_TYPE_LLC)
     {
         memcpy(&(LLC.DSAP), frame_start, DSAP_SIZE);
-        memcpy(&(LLC.DSAP), frame_start + DSAP_SIZE, SSAP_SIZE);
+        memcpy(&(LLC.SSAP), frame_start + DSAP_SIZE, SSAP_SIZE);
         if(LLC_control_size == CONTROL_SIZE_16)
         {
             memcpy(&(LLC.control), frame_start + DSAP_SIZE + SSAP_SIZE, LLC_control_size);
@@ -119,7 +125,7 @@ void process_LLC(const u_char *frame_start, uint16_t data_length)
         }
         
         
-        LLC_SNAP.data_start = frame_start + DSAP_SIZE + SSAP_SIZE + LLC_control_size;
-        LLC_SNAP.data_length = data_length - (DSAP_SIZE + SSAP_SIZE + LLC_control_size);
+        LLC.data_start = frame_start + DSAP_SIZE + SSAP_SIZE + LLC_control_size;
+        LLC.data_length = data_length - (DSAP_SIZE + SSAP_SIZE + LLC_control_size);
     }
 }
