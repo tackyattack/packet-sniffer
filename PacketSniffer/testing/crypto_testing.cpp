371a2b36ccc0566b53a19b6220cb949aa729d7ad
HENRY BERGIN <tackyattack@gmail.com>
2017-11-10 20:40:32 -0600

    added test vectors

--- PacketSniffer/testing/crypto_testing.cpp
+++ PacketSniffer/testing/crypto_testing.cpp
@@ -11,19 +11,53 @@
 #include "SHA_1_hash.h"
 #include "btest.h"
 
+void clear_str(char *output)
+{
+    for(uint32_t i = 0; i < 40; i++)
+    {
+        output[i] = 0;
+    }
+}
 
 TEST(crypto, SHA_1_hash)
 {
-    char test_hash[] = "The quick brown fox jumps over the lazy dog";
-    // should get: 2fd4e1c67a2d28fced849ee1bb76e7391b93eb12
-    char test_hash2[] = "abc";
-    // should get: a9993e36 4706816a ba3e2571 7850c26c 9cd0d89d
-    char test_hash3[] = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu";
-    // should get: a49b2446 a02c645b f419f995 b6709125 3a04a259
-    
     char output[40] = {0};
     char output_bytes[20] = {0};
-    SHA_1_hash(test_hash, output, output_bytes, strlen(test_hash));
+    
+    char test_hash1[] = "The quick brown fox jumps over the lazy dog";
+    // should get: 2fd4e1c67a2d28fced849ee1bb76e7391b93eb12
+    char test_vec_1[] = "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12";
+    
+    char test_hash2[] = "abc";
+    // should get: a9993e36 4706816a ba3e2571 7850c26c 9cd0d89d
+    char test_vec_2[] = "a9993e364706816aba3e25717850c26c9cd0d89d";
+    
+    char test_hash3[] = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu";
+    // should get: a49b2446 a02c645b f419f995 b6709125 3a04a259
+    char test_vec_3[] = "a49b2446a02c645bf419f995b67091253a04a259";
+    
+    char test_hash4[] = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq";
+    // should get: a49b2446 a02c645b f419f995 b6709125 3a04a259
+    char test_vec_4[] = "a49b2446a02c645bf419f995b67091253a04a259";
+    
+    
+    SHA_1_hash(test_hash1, output, output_bytes, (uint32_t)strlen(test_hash1));
+    EXPECT_EQ_STR(output, test_vec_1, strlen(test_vec_1));
+    clear_str(output);
+    
+    SHA_1_hash(test_hash2, output, output_bytes, (uint32_t)strlen(test_hash2));
+    EXPECT_EQ_STR(output, test_vec_2, strlen(test_hash2));
+    clear_str(output);
+    
+    SHA_1_hash(test_hash3, output, output_bytes, (uint32_t)strlen(test_hash3));
+    EXPECT_EQ_STR(output, test_vec_3, strlen(test_vec_3));
+    clear_str(output);
+    
+    SHA_1_hash(test_hash4, output, output_bytes, (uint32_t)strlen(test_hash4));
+    EXPECT_EQ_STR(output, test_vec_4, strlen(test_vec_4));
+    clear_str(output);
+    
+    
 }
 
 void init_crypto_testing()
