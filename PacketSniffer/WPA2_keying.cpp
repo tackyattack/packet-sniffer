55b80828d54ec1bac5ccbf370d70c79cfda45f68
HENRY BERGIN <tackyattack@gmail.com>
2017-12-21 19:52:25 -0600

    concat

--- PacketSniffer/WPA2_keying.cpp
+++ PacketSniffer/WPA2_keying.cpp
@@ -45,87 +45,42 @@ void xor_str_self(char *strA, char *strB, uint16_t length)
     }
 }
 
-// function F
-
-//F (P, S, c, i) = U_1 \xor U_2 \xor ... \xor U_c
-//
-//where
-//
-//U_1 = PRF (P, S || INT (i)) ,
-//U_2 = PRF (P, U_1) ,
-//...
-//U_c = PRF (P, U_{c-1})
-
-// My algorithm:
-//
-// input = salt || INT(i)
-//
-// B = input
-// loop()
-// {
-//    A = hash(B)
-//    B = A
-//    if(first time around)
-//    {
-//       last_xor = A
-//    }
-//    else
-//    {
-//       last_xor = last_xor ^ A
-//     }
-// }
-// end result: last_xor is the xor sum
-
-
-
 //"f4 2c 6f c5 2d f0ebef9ebb4b90b38a5f902e83fe1b135a70e23aed762e9710a12e"
 // http://anandam.name/pbkdf2/
 void exclusive_or_sum(char *P, char *S, uint16_t c, uint16_t i, char *output)
 {
     char U_x[20] = {0};
-    char U_1[20] = {0};
-    char U_2[20] = {0};
     char U_last[20] = {0};
     char U_o[20] = {0}; // xor result
-    uint8_t sz = strlen(S) + 4 + 1;
+    uint8_t sz = strlen(S) + 4;
     char *S_cat = (char *)malloc(sz);
     strcpy(S_cat, S);
     uint16_t end_of_S_index = strlen(S_cat);
     S_cat[end_of_S_index]   = 0x00 + 0;
     S_cat[end_of_S_index+1] = 0x00 + 0;
     S_cat[end_of_S_index+2] = ((i & 0xff00) >> 8) + 0;
-    S_cat[end_of_S_index+3] = (i & 0x00ff)        + 0; // this will break down once >9
-    S_cat[end_of_S_index+4] = '\0';
-    uint8_t xx = strlen(S_cat);
-    HMAC(P, strlen(P), S_cat, 8, U_x); // IEEE + 0001
+    S_cat[end_of_S_index+3] = (i & 0x00ff)        + 0;
+   
+    HMAC(P, strlen(P), S_cat, sz, U_x); // get the first U_x with the salt
     memcpy(U_last, U_x, 20);
     memcpy(U_o, U_x, 20);
-    uint16_t cnt = 1;
 
     for(uint16_t c_cnt = 0; c_cnt < (c-1); c_cnt++)
-    { // check to see if all of this logic makes sense
+    {
         HMAC(P, strlen(P), U_last, sizeof(U_last), U_x);
         xor_str_self(U_o, U_x, 20);
         memcpy(U_last, U_x, 20);
-        cnt++;
     }
     
-//    HMAC(P, strlen(P), S_cat, strlen(S_cat), U_1);
-//    HMAC(P, strlen(P), U_1, sizeof(U_1), U_2); // make sure strlen is working '\0'
-//    memcpy(U_last, U_2, 20);
-//    xor_str(U_1, U_2, U_o, 20);
-//    for(uint16_t i = 0; i < c; i++) // start ahead because we aleady did first
-//    {
-//        HMAC(P, strlen(P), U_last, sizeof(U_last), U_1);
-//        xor_str(U_last, U_1, U_o, 20); // should probably switch xor to modify the input instead of making an output variable
-//    }
-//
-//    memcpy(output, U_o, sizeof(U_o));
+    memcpy(output, U_o, sizeof(U_o));
 }
 
-void F(char *P, char *S, uint16_t c, uint16_t i, char *output)
+void cat_byte(char *dest, uint16_t offset, char *src, uint16_t len)
 {
-    //char U_x[20] = {0};
+    for(uint16_t i = 0; i < len; i++)
+    {
+        dest[offset+i] = src[i];
+    }
 }
 
 void PBKDF2(char *P, char *S, uint16_t c, uint16_t dkLen, char *output)
@@ -149,13 +104,23 @@ void PBKDF2(char *P, char *S, uint16_t c, uint16_t dkLen, char *output)
     // DK = T_1 || T_2 ||  ...  || T_l<0..r-1>
     // With the last block being r bytes long
     
-    uint16_t i = 1;
     char hBlock[hLen] = {0};
-    //"f4 2c 6f c5 2d f0ebef9ebb4b90b38a5f902e83fe1b135a70e23aed762e9710a12e"
-    for(; i <= l; i++)
+    uint8_t cpy_len = 0;
+    //"f42c6fc52df0ebef9ebb4b90b38a5f902e83fe1b135a70e23aed762e9710a12e"
+    for(uint16_t i = 0; i < l; i++)
     {
-        exclusive_or_sum(P, S, c, i, hBlock);
-        strcat(output, hBlock);
+        exclusive_or_sum(P, S, c, i+1, hBlock);
+        
+        if(i == (l-1))
+        {
+            cpy_len = r;
+        }
+        else
+        {
+            cpy_len = hLen;
+        }
+        
+        cat_byte(output, (i)*hLen, hBlock, cpy_len);
     }
     
     
